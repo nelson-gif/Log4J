17b1f857b87e61a4d748279b5a84093d840cfc3c
nelson-gif <nelsonvelasquez2.nv@gmail.com>
2024-06-03 19:20:05 -0600

    fisrt commit

---
+++ src/main/java/com/log4j/ejemploLog4J/App.java
@@ -0,0 +1,13 @@
+package com.log4j.ejemploLog4J;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
